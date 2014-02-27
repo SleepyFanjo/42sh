cc004480a57f1d9d5d932f476472c1bb721df6d1
Quentin CHEVRIN <quentin.chevrin@gmail.com>
2014-02-27 16:25:05 +0100

    Lexer : Token generee et identifie, main de test, makefile

--- parser_mofo/parser.h
+++ parser_mofo/parser.h
@@ -6,7 +6,7 @@
 /*   By: qchevrin <qchevrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/24 11:16:46 by qchevrin          #+#    #+#             */
-/*   Updated: 2014/02/26 17:51:11 by qchevrin         ###   ########.fr       */
+/*   Updated: 2014/02/27 16:16:11 by qchevrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,21 @@
 # include <libft.h>
 # include <struct.h>
 
-int			q_is_special_char(char *str, int i);
+int			q_is_special_char(char *str, int i, int *end);
+int			q_count_normal(char *str, int *beg, int *end);
+int			q_count_token_len(char *str, int *beg, int *end, int *inh);
 int			q_is_whitespace(char c);
-char		*g_strndup(char *str, int n);
+char		*q_strndup(char *str, int n, int inh);
 void		q_error(char *s1, char *s2, int mode);
 t_list		*q_create_obj(void *elem);
-void		q_add_in_list(t_list **list, void *elem);
+int			q_add_in_list(t_list **list, void *elem);
 t_token		*q_create_token(char *str, int *i);
+t_token		*q_white_token(char *str, int *i);
+t_token		*q_utility_token(char *name);
 t_list		*q_lexer(char *str);
+void		q_free_list(t_list **list);
+
+/* ***** */
+char		*tmp_prompt(void);
 
 #endif
--- parser_mofo/q_count_token_len.c
+++ parser_mofo/q_count_token_len.c
@@ -6,7 +6,7 @@
 /*   By: qchevrin <qchevrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/26 14:24:50 by qchevrin          #+#    #+#             */
-/*   Updated: 2014/02/26 17:51:08 by qchevrin         ###   ########.fr       */
+/*   Updated: 2014/02/27 15:38:31 by qchevrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,23 @@
 
 static int	is_separator(char *str, int i)
 {
-	if (q_is_whitespace(str[i]) || q_is_special_char(str, i))
+	if (q_is_whitespace(str[i]) || q_is_special_char(str, i, NULL))
 		return (1);
 	if (str[i] == '\0')
 		return (1);
+	return (0);
 }
 
-int			q_is_special_char(char *str, int i)
+int			q_is_special_char(char *str, int i, int *end)
 {
 	if (str[i] == '<' || str[i] == '>' || str[i] == '&' || str[i] == '|')
 	{
 		if (str[i + 1] == str[i])
+		{
+			if (end != NULL)
+				*end = *end + 1;
 			return (2);
+		}
 		return (1);
 	}
 	return (0);
@@ -33,29 +38,46 @@ int			q_is_special_char(char *str, int i)
 
 int			q_count_normal(char *str, int *beg, int *end)
 {
+	int		i;
+
+	i = 0;
 	while (!is_separator(str, *end))
+	{
+		if (str[*end] == '\\')
+		{
+			i = i + 1;
+			if (str[*end + 1] != '\0')
+				*end = *end + 1;
+		}
 		*end = *end + 1;
-	return (*end - *beg);
+	}
+	*end = *end - 1;
+	return (*end - *beg + 1 - i);
 }
 
-int			q_count_token_len(char *str, int *beg, int *end)
+int			q_count_token_len(char *str, int *beg, int *end, int *inh)
 {
 	char	c;
 	int		k;
 
-	if (str[beg] == '\"' || str[beg] == '\'')
+	*inh = 0;
+	if (str[*beg] == '\"' || str[*beg] == '\'')
 	{
+		*inh = 1;
 		c = str[*beg];
 		*beg = *beg + 1;
-		while (str[*end + 1] != '\0' &&
-				(str[*end + 1] != c || str[*end] == '\\'))
+		while (str[*end + 1] != c || str[*end] == '\\')
+		{
+			if (str[*end] == '\0')
+				return (-1);
 			*end = *end + 1;
-		k = *end - beg + 1;
+		}
+		k = *end - *beg + 1;
 		if (str[*end + 1] == c)
 			*end = *end + 1;
 		return (k);
 	}
-	if ((k = q_is_special_char(str, *beg)) > 0)
+	if ((k = q_is_special_char(str, *beg, end)) > 0)
 		return (k);
 	else
 		return (q_count_normal(str, beg, end));
