2e38f6fd228fe0ff508e2df2a39a8bdceececc16
Quentin CHEVRIN <quentin.chevrin@gmail.com>
2014-03-07 15:51:27 +0100

    resolution d'une petite erreur de compilation (fuck clang)

--- parser_mofo/q_parser.c
+++ parser_mofo/q_parser.c
@@ -6,7 +6,7 @@
 /*   By: qchevrin <qchevrin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/02/28 14:02:59 by qchevrin          #+#    #+#             */
-/*   Updated: 2014/03/05 12:06:00 by qchevrin         ###   ########.fr       */
+/*   Updated: 2014/03/07 15:31:16 by qchevrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,10 @@ t_cmd		*q_fill_cmd(t_list **token_list)
 
 t_list		*q_parser(t_list *token_list)
 {
-	t_list	*save;
 	t_list	*list;
 	int		error;
 
 	list = NULL;
-	save = token_list;
 	while (token_list != NULL)
 	{
 		error = q_add_in_list(&list, (void *)q_fill_cmd(&token_list));
